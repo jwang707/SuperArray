e075f328cfcf179ca3a65788ca3cafed790032e3
Jasmine <jwang2367@gmail.com>
2020-11-07 18:38:20 -0500

    zip works :)

--- Demo.java
+++ Demo.java
@@ -19,6 +19,26 @@ public class Demo{
     return kachow;
   }
 
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    SuperArray unusAnnus = new SuperArray();
+    for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
+      unusAnnus.add(a.get(i));
+      unusAnnus.add(b.get(i));
+    }
+    if (a.size() > b.size()) {
+      for (int i = b.size(); i < a.size(); i++) {
+        unusAnnus.add(a.get(i));
+      }
+    }
+    else if (b.size() > a.size()) {
+      for (int i = a.size(); i < b.size(); i++) {
+        unusAnnus.add(b.get(i));
+    }
+  }
+    return unusAnnus;
+  }
+
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -27,14 +47,24 @@ public class Demo{
     words.add("una");    words.add("ebi");     words.add("toro");
 
     System.out.println(words);
+    System.out.println(words.lastIndexOf("ebi"));
     removeDuplicates(words);
     System.out.println("\n");
     System.out.println(words);
+    System.out.println(words.lastIndexOf("ebi"));
+
+    SuperArray lilWords = new SuperArray();
+    lilWords.add("kani");   lilWords.add("ebi");  lilWords.add("toro");
 
     SuperArray newWords = new SuperArray();
     newWords.add("kani");   newWords.add("ebi");  newWords.add("toro");
 
     System.out.println(newWords.toString());
     System.out.println((findOverlap(words, newWords)).toString());
+    System.out.println(newWords.equals(words)); //false
+    System.out.println(newWords.equals(lilWords)); //true
+    System.out.println(zip(lilWords, newWords)); //true
+
+
   }
 }
